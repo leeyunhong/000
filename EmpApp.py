ee3423c1b7ec14aa9226a27962e38692c7bd5b96
leeyunhong <leeashton115@gmail.com>
2022-09-28 00:18:28 +0800

    up

--- EmpApp.py
+++ EmpApp.py
@@ -112,11 +112,24 @@ def UpdateEmp(emp_id):
 
 @app.route("/fetchdata", methods=['GET','POST'])
 def FetchEmp():
+    select_sql = """SELECT * FROM employees"""
     cursor = db_conn.cursor()
-    cursor.execute("SELECT * FROM employees WHERE emp_id")
+    cursor.execute(select_sql)
     FetchEmp = cursor.fetchall()
     cursor.close()
-    return render_template('GetEmpOutput.html', id=emp_id, fname=first_name, lname=last_name, interest=pri_skill, location=location)
+    p = []
+    tbl = "<tr><td>ID</td><td>FirstName</td><td>LastName</td><td>Interest</td><td>Location</td></tr>"
+    p.append(tbl)
+    for row in result:
+    a = "<tr><td>%s</td>"%row[0]
+    p.append(a)
+    b = "<td>%s</td>"%row[1]
+    p.append(b)
+    c = "<td>%s</td>"%row[2]
+    p.append(c)
+    d = "<td>%s</td></tr>"%row[3]
+    p.append(d)
+    return render_template('GetEmpOutput.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=True)
